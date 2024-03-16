972147adcd9b04b23b2cc2b9ea96ccbb942ebcc8
Fabiola Jessica Huancollo Chambi <hcjessica@outlook.com>
2024-03-16 14:59:51 -0500

    13. matriculas ok

---
+++ 13-cjava-cursos/src/app/matricula/matricula/matricula.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatriculaComponent } from './matricula.component';
+
+describe('MatriculaComponent', () => {
+  let component: MatriculaComponent;
+  let fixture: ComponentFixture<MatriculaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatriculaComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MatriculaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
